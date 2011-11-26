e7e8728bd3b1a0a47d250a25233cbdae3065cfa1
Matthias Derer <bestform@tonstube.de>
2011-11-26 22:01:44 +0100

    added new feature: make a link from an existing URL. The plugin tries to be clever and find out the title of the page. If it does, it is nice enough to set it for you.

--- anchorman.py
+++ anchorman.py
@@ -1,7 +1,8 @@
 import sublime
 import sublime_plugin
 import json
-from urllib import urlopen, quote
+from urllib2 import urlopen, quote, Request
+import re
 
 class AnchormanCommand(sublime_plugin.TextCommand):
   def run(self, edit, multiple):
@@ -26,7 +27,7 @@ class AnchormanCommand(sublime_plugin.TextCommand):
       return
     
     self.originalQuery = self.view.substr(self.region)
-    query = self.cleanQuery(self.originalQuery)
+    query = Utils.cleanQuery(self.originalQuery)
     url = baseurl + quote(query.encode("utf-8"))
     result = urlopen(url).read()
     # no error handling at this point. we trust microsoft. i for one welcome our new api overlords.
@@ -36,7 +37,7 @@ class AnchormanCommand(sublime_plugin.TextCommand):
       return
     if not multiple:
       resulturl = results[0]["Url"]
-      self.writeLink(resulturl, self.originalQuery)
+      Utils.writeLink(self, resulturl, self.originalQuery)
       return
     for i in range(0, min(len(results), count)):
       self.items.append([results[i]["Title"], results[i]["Url"]])
@@ -45,25 +46,48 @@ class AnchormanCommand(sublime_plugin.TextCommand):
   def panelDone(self, index):
     if index == -1:
       return
-    self.writeLink(self.items[index][1], self.originalQuery)
+    Utils.writeLink(self, self.items[index][1], self.originalQuery)
 
-  def writeLink(self, resulturl, originalquery):
-    edit = self.view.begin_edit()
-    start = self.region.begin()
-    end = self.region.end()
+class AnchormanLinkCommand(sublime_plugin.TextCommand):
+  def run(self, edit):
+    self.region = self.view.sel()[0]
+    if self.region.size() == 0:
+      # nothing selected? get off my lawn!
+      return
+    url = self.view.substr(self.region)
+    if not url.startswith("http"):
+      # no URL? oh c'mon
+      return
+
+    titleRegEx = re.compile("<title.*?>(.*?)</title>", re.I|re.M)
+    user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.106 Safari/535.2"
+    headers = {'User-Agent': user_agent}
+    request = Request(url, None, headers)
+    response = urlopen(request).read()
+    match = titleRegEx.search(response)
+    title = url
+    if match.group(1):
+      title = match.group(1)
+    Utils.writeLink(self, url, title)
+
+class Utils(object):
+  @staticmethod
+  def writeLink(source, resulturl, title):
+    edit = source.view.begin_edit()
+    start = source.region.begin()
+    end = source.region.end()
     part1 = "<a href=\""+resulturl+"\">"
-    part2 = originalquery
+    part2 = title
     part3 = "</a>"
     anchor = part1+part2+part3
     markstart = start + len(part1)
     markend = markstart + len(part2)
-
-    self.view.replace(edit, self.region, anchor)
-    self.view.sel().clear()
-    self.view.sel().add(sublime.Region(markstart, markend))
-
-    self.view.end_edit(edit)
-
-  def cleanQuery(self, query):
+    source.view.replace(edit, source.region, anchor)
+    source.view.sel().clear()
+    source.view.sel().add(sublime.Region(markstart, markend))
+    source.view.end_edit(edit)
+  
+  @staticmethod
+  def cleanQuery(query):
     query = query.replace(" ", "+")
     return query
